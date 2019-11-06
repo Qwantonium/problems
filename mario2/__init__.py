a9aa675a4405af1abd95353cf9624744c6780e6e
ACG Parnell College <57234021+ACGparnell@users.noreply.github.com>
2019-11-07 11:51:24 +1300

    Update __init__.py

--- mario2/__init__.py
+++ mario2/__init__.py
@@ -1,4 +1,4 @@
-less = __import__("check50").import_checks("../less")
+less = __import__("check50").import_checks("../mario1")
 from less import *
 
 # mario/more checks are identical to the mario/less checks just with different txt files
