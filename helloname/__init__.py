ec096711199537f0224217ffd0fea725cff86157
ACG Parnell College <57234021+ACGparnell@users.noreply.github.com>
2019-11-08 07:56:24 +1300

    Update and rename hello_person/__init__.py to helloname/__init__.py

--- helloname/__init__.py
+++ helloname/__init__.py
@@ -3,14 +3,14 @@ import check50
 @check50.check()
 def exists():
     """hello.py exists."""
-    check50.exists("hello.py")
+    check50.exists("helloname.py")
 
 @check50.check(exists)
 def veronica():
     """responds to name James."""
-    check50.run("python3 hello.py").stdin("James").stdout("James").exit(0)
+    check50.run("python3 helloname.py").stdin("James").stdout("James").exit(0)
 
 @check50.check(exists)
 def brian():
     """responds to name Phoebe."""
-    check50.run("python3 hello.py").stdin("Phoebe").stdout("Phoebe").exit(0)
+    check50.run("python3 helloname.py").stdin("Phoebe").stdout("Phoebe").exit(0)
